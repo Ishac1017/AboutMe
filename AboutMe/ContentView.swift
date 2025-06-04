157dc22ca73cacb5f4f0250d90d6c59f8e7bd8b2
Isha <ishac1017@gmail.com>
2025-06-04 14:01:03 -0500

    Made very basic template for app.

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,15 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            VStack() {
+                Image("grad")
+                    .resizable(resizingMode: .stretch)
+                    .clipShape(Circle())
+                    .aspectRatio(contentMode: .fit)
+                    //.frame(width: 400.0, height: 400.0)
+                
+                Text("Welcome to my About Me App! I am Isha. I recently graduated highschool and am excited to start my new journey at S&T! This App showcases my involvement in Highschool.")
+                    .frame(width: 350.0, height: 150.0)
+                    .background(.white)
+                    
+            }
+            //end VStack
+            .padding(.top, 5.0)
+            .frame(width: 500.0, height: 500.0)
+            .background(.pink)
+            
+            VStack() {
+                
+            }
+            .frame(width: 400.0, height: 400.0)
+            .background(.blue)
+
+            
         }
-        .padding()
+        //end scrollview
     }
-}
+    //end body
+}//end content
 
 #Preview {
     ContentView()
